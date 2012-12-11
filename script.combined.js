14c6e552d9b4a794ab3742bec98b743056225795
Ali Mihandoost <i@ali.md>
2012-12-11 11:38:33 +0330

    Remove colorbox

--- script.combined.js
+++ script.combined.js
@@ -1,7 +1,8 @@
 <!--#include file="scripts/html5shiv.js" -->
 if('__proto__' in {}) {
 	<!--#include file="scripts/zepto.js" -->
+}else{
+	<!--#include file="scripts/jquery.js" -->
 }
-<!--#include file="scripts/jquery.js" -->
-<!--#include file="scripts/colorbox.js" -->
+// TODO: Improved load performance.
 <!--#include file="scripts/script.js" -->
