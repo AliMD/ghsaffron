a37983fb415842b8c7665ab6bbe717455c2c38e1
Ali Mihandoost <i@ali.md>
2012-12-30 10:17:19 +0330

    backup old layout and renew

---
+++ pages/backup.php
@@ -0,0 +1,97 @@
+<header>
+	<section class="banner">
+		<nav>
+			<ul>
+				<li class="item1">
+					<a href="#">home</a>
+				</li>
+				<li class="item2">
+					<a href="#">products</a>
+				</li>
+				<li class="item3">
+					<a href="#">certificates</a>
+				</li>
+				<li class="item4">
+					<a href="#">about us</a>
+				</li>
+				<li class="item5">
+					<a href="#">branches</a>
+				</li>
+				<li class="item6">
+					<a href="#">contact us</a>
+				</li>
+			</ul>
+		</nav>
+		<h1 class="logo"><img src="images/logo.png" width="201" height="181" alt="Ghasem Saffron" /></h1>
+	</section>
+</header>
+<div class="content_wrap">
+	<section class="home">HOME
+		<div class="description">
+			<h2></h2>
+			<p></p>
+		</div>
+	</section>
+	<section class="products_list">products_list
+		<h2></h2>
+		<div class="line"></div>
+		<section class="cat_list mla mra">
+			<div class="cat_01 left"></div>
+			<div class="cat_02 right"></div>
+			<div class="cat_03"></div>
+		</section>
+	</section>
+	<section class="product">
+		<div class="prd_container wx">
+			<div class="prd_01 mr2">
+				<div class="description left">
+					<h2></h2>
+					<p></p>
+				</div>
+			</div>
+			<div class="prd_02">
+				<div class="description left">
+					<h2></h2>
+					<p></p>
+				</div>
+			</div>
+			<div class="prd_03 ml2">
+				<div class="description left">
+					<h2></h2>
+					<p></p>
+				</div>
+			</div>
+		</div>
+		<div class="next"></div>
+		<div class="back"></div>
+	</section>
+	<section class="certificates">
+		<div class="scroll wx">
+			<div class="cert_01 mr1">
+				<div class="description left">
+					<h2></h2>
+					<p></p>
+				</div>
+			</div>
+			<div class="cert_02">
+				<div class="description left">
+					<h2></h2>
+					<p></p>
+				</div>
+			</div>
+			<div class="cert_03 ml1">
+				<div class="description left">
+					<h2></h2>
+					<p></p>
+				</div>
+			</div>
+		</div>
+		<section class="thumb">
+			<div class="cert_tumb_01"></div>
+			<div class="cert_tumb_02"></div>
+			<div class="cert_tumb_03 last mla"></div>
+		</section>
+		<div class="next"></div>
+		<div class="back"></div>
+	</section>
+</div>
