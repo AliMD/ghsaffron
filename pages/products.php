91a19d8ade753acc96bba90de58441e6c9866948
Ali aGHasizadeh <me@aligh.pro>
2013-01-06 13:03:03 +0330

    Add Prd basic layout

---
+++ pages/products.php
@@ -0,0 +1,37 @@
+<section class="prd-container">
+	<div class="col1">
+		<div class="prd-1">
+			<img src="images/prd/1.jpg" />
+			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
+		</div>
+		<div class="prd-2">
+			<img src="images/prd/1.jpg" />
+			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
+		</div>
+		<div class="prd-3">
+			<img src="images/prd/1.jpg" />
+			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
+		</div>
+		<div class="prd-4">
+			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
+		
+		<img src="images/prd/1.jpg" /></div>
+	</div>
+	<div class="col2">
+		<div class="prd-5">
+			<img src="images/prd/1.jpg" />
+			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
+		</div>
+		<div class="prd-6">
+			<img src="images/prd/1.jpg" />
+			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
+		</div>
+		<div class="prd-7">
+			<img src="images/prd/1.jpg" />
+			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
+		</div>
+		<div class="prd-8">
+			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
+		</div>
+	</div>
+</section>
