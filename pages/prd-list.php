d0d0806163307ae3190aa802ee791ce03c34f6ce
Ali aGHasizadeh <me@aligh.pro>
2013-01-06 14:21:01 +0330

    Add prd-list page

---
+++ pages/prd-list.php
@@ -0,0 +1,10 @@
+<section class="prd-list-container">
+	<div class="prd-list-1">
+		<div class="item-list">
+			<div class="item1"></div>
+			<div class="item2"></div>
+			<div class="item3"></div>
+		</div>
+		<div class="prd-txt"></div>
+	</div>
+</section>
