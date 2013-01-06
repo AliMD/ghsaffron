82176f8c702f981e86476e8c7763a87c3c8de0b6
Ali aGHasizadeh <me@aligh.pro>
2013-01-06 16:38:23 +0330

    Better style and Structure

--- pages/products.php
+++ pages/products.php
@@ -1,38 +1,40 @@
 <section class="prd-container">
-	<div class="col1">
+	<div class="prd-title">Products</div>
+	<div class="col1 cover">
 		<div class="prd-1">
-			<img src="images/prd/1.png" width="202" height="202" />
+			<img src="images/prd/1.png" width="120" height="120" />
 			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
 		</div>
 		<div class="prd-2">
-			<img src="images/prd/1.png" width="202" height="202" />
+			<img src="images/prd/1.png" width="120" height="120" />
 			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
 		</div>
 		<div class="prd-3">
-			<img src="images/prd/1.png" width="202" height="202" />
+			<img src="images/prd/1.png" width="120" height="120" />
 			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
 		</div>
 		<div class="prd-4">
 			<span class="prd-hover"><?php echo rand(10, 18); ?></span>
 		
-		<img src="images/prd/1.png" width="202" height="202" /></div>
+		<img src="images/prd/1.png" width="120" height="120" /></div>
 	</div>
-	<div class="col2">
+	<div class="col2 cover">
 		<div class="prd-5">
-			<img src="images/prd/1.png" width="202" height="202" />
+			<img src="images/prd/1.png" width="120" height="120" />
 			<span class="prd-hover"><?php echo rand(10, 18); ?></span>
 		</div>
 		<div class="prd-6">
-			<img src="images/prd/1.png" width="202" height="202" />
+			<img src="images/prd/1.png" width="120" height="120" />
 			<span class="prd-hover"><?php echo rand(10, 18); ?></span>
 		</div>
 		<div class="prd-7">
-			<img src="images/prd/1.png" width="202" height="202" />
+			<img src="images/prd/1.png" width="120" height="120" />
 			<span class="prd-hover"><?php echo rand(10, 18); ?></span>
 		</div>
 		<div class="prd-8">
-			<img src="images/prd/1.png" width="202" height="202" />
+			<img src="images/prd/1.png" width="120" height="120" />
 			<span class="prd-hover"><?php echo rand(10, 18); ?></span>
 		</div>
 	</div>
+	<div class="prd-shadow"></div>
 </section>
