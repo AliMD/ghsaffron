a972b587e2981512fc190d63e11d7caa8bdb18d9
Ali aGHasizadeh <me@aligh.pro>
2013-01-06 13:18:37 +0330

    change size and img

--- pages/products.php
+++ pages/products.php
@@ -1,36 +1,37 @@
 <section class="prd-container">
 	<div class="col1">
 		<div class="prd-1">
-			<img src="images/prd/1.jpg" />
+			<img src="images/prd/1.png" width="202" height="202" />
 			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
 		</div>
 		<div class="prd-2">
-			<img src="images/prd/1.jpg" />
+			<img src="images/prd/1.png" width="202" height="202" />
 			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
 		</div>
 		<div class="prd-3">
-			<img src="images/prd/1.jpg" />
+			<img src="images/prd/1.png" width="202" height="202" />
 			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
 		</div>
 		<div class="prd-4">
 			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
 		
-		<img src="images/prd/1.jpg" /></div>
+		<img src="images/prd/1.png" width="202" height="202" /></div>
 	</div>
 	<div class="col2">
 		<div class="prd-5">
-			<img src="images/prd/1.jpg" />
+			<img src="images/prd/1.png" width="202" height="202" />
 			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
 		</div>
 		<div class="prd-6">
-			<img src="images/prd/1.jpg" />
+			<img src="images/prd/1.png" width="202" height="202" />
 			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
 		</div>
 		<div class="prd-7">
-			<img src="images/prd/1.jpg" />
+			<img src="images/prd/1.png" width="202" height="202" />
 			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
 		</div>
 		<div class="prd-8">
+			<img src="images/prd/1.png" width="202" height="202" />
 			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
 		</div>
 	</div>
