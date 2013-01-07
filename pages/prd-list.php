2adc6b746939cff085020f7721ebbbf725c2797a
Ali aGHasizadeh <me@aligh.pro>
2013-01-07 11:38:01 +0330

    better Structure

--- pages/prd-list.php
+++ pages/prd-list.php
@@ -1,9 +1,11 @@
 <section class="prd-det1">
-	<div class="title"></div>
-	<div class="pic">
+	<div class="title">
+		<h2></h2>
+		<p></p>
+	</div>
+	<div class="pics">
 		<div class="pic1"></div>
 		<div class="pic2"></div>
 		<div class="pic3"></div>
 	</div>
-	<div class="txt"></div>
 </section>
