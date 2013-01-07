83b9de113556d597335ad508fb417a620792ff71
Ali aGHasizadeh <me@aligh.pro>
2013-01-07 12:28:05 +0330

    Add sample TXT

--- pages/prd-list.php
+++ pages/prd-list.php
@@ -1,7 +1,7 @@
 <section class="prd-det1">
 	<div class="title">
-		<h2></h2>
-		<p></p>
+		<h2>Package</h2>
+		<p>Holding your phone, a couple of credit cards and banknotes - this protective case is all you need for a day in the city or a night out on the tiles. Measures 8.5cm x 12cm.</p>
 	</div>
 	<div class="pics">
 		<div class="pic1"></div>
