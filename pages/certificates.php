00a4ea3736d4d29784f5df26173320357913b9c2
Ali aGHasizadeh <me@aligh.pro>
2013-01-07 13:44:46 +0330

    Add Certificates

---
+++ pages/certificates.php
@@ -0,0 +1,38 @@
+<section class="certificates-container">
+	<div class="background_holder right">
+		<div class="cert_01">
+			<div class="description left">
+				<h2></h2>
+				<p></p>
+			</div>
+		</div>
+		<div class="cert_02">
+			<div class="description left">
+				<h2></h2>
+				<p></p>
+			</div>
+		</div>
+		<div class="cert_03">
+			<div class="description left">
+				<h2></h2>
+				<p></p>
+			</div>
+		</div>
+		<div class="cert_04">
+			<div class="description left">
+				<h2></h2>
+				<p></p>
+			</div>
+		</div>
+	</div>
+	<section class="thumb_holder mla wx">
+		<div class="cert_tumb_01 left ml1"></div>
+		<div class="cert_tumb_02 left ml1"></div>
+		<div class="cert_tumb_03 left ml1"></div>
+		<div class="cert_tumb_04 left ml1"></div>
+	</section>
+	<div class="title">
+		<h2>Certificates</h2>
+		<p>Holding your phone, a couple of credit cards and banknotes - this protective case is all you need for a day in the city or a night out on the tiles. Measures 8.5cm x 12cm.</p>
+	</div>
+</section>
