4e7ceb39e42ac8f63288e954fdc919f37bb769c1
AliGH <me@aligh.pro>
2013-02-02 09:28:49 +0330

    add logo-menu_fa

---
+++ pages/logo-menu_fa.php
@@ -0,0 +1,29 @@
+<header>
+	<section class="banner">
+		<nav>
+			<ul>
+				<li class="item1">
+					<a href="#">خانه</a>
+				</li>
+				<li class="item2">
+					<a href="#">محصولات</a>
+				</li>
+				<li class="item3">
+					<a href="#">مجوز ها</a>
+				</li>
+				<li class="item4">
+					<a href="#">درباره ما</a>
+				</li>
+				<li class="item5">
+					<a href="#">برنچ ها</a>
+				</li>
+				<li class="item6">
+					<a href="#">ارتباط با ما</a>
+				</li>
+			</ul>
+		</nav>
+		<h1 class="logo">
+			<img src="images/logo.png" width="201" height="181" alt="Ghasem Saffron" />
+		</h1>
+	</section>
+</header>
