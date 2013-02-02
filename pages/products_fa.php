bf2ffa0c61c13ecdd6b433e43f442b79aaf16522
AliGH <me@aligh.pro>
2013-02-02 10:00:43 +0330

    Add product_fa

---
+++ pages/products_fa.php
@@ -0,0 +1,39 @@
+<section class="prd-container">
+	<div class="prd-title">محصولات</div>
+	<div class="prds cover">
+		<div class="prd-1">
+			<img src="images/prd/1.png" width="120" height="100" />
+			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
+		</div>
+		<div class="prd-2">
+			<img src="images/prd/1.png" width="120" height="100" />
+			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
+		</div>
+		<div class="prd-3">
+			<img src="images/prd/1.png" width="120" height="100" />
+			<span class="prd-hover"><?php echo rand(10, 20); ?></span>
+		</div>
+		<div class="prd-4">
+			<span class="prd-hover"><?php echo rand(10, 18); ?></span>
+		
+		<img src="images/prd/1.png" width="120" height="100" />
+		</div>
+		<div class="prd-5">
+			<img src="images/prd/1.png" width="120" height="100" />
+			<span class="prd-hover"><?php echo rand(10, 18); ?></span>
+		</div>
+		<div class="prd-6">
+			<img src="images/prd/1.png" width="120" height="100" />
+			<span class="prd-hover"><?php echo rand(10, 18); ?></span>
+		</div>
+		<div class="prd-7">
+			<img src="images/prd/1.png" width="120" height="100" />
+			<span class="prd-hover"><?php echo rand(10, 18); ?></span>
+		</div>
+		<div class="prd-8">
+			<img src="images/prd/1.png" width="120" height="100" />
+			<span class="prd-hover"><?php echo rand(10, 18); ?></span>
+		</div>
+	</div>
+	<div class="prd-shadow"></div>
+</section>
