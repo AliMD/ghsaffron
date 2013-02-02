c22028cf83c37ef763192ed844012e8dcead7e67
AliGH <me@aligh.pro>
2013-02-02 08:56:34 +0330

    add fa header

---
+++ inc/header-fa.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>		<html lang="fa-IR" dir="rtl" class="lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>			<html lang="fa-IR" dir="rtl" class="lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>			<html lang="fa-IR" dir="rtl" class="lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->	<html lang="fa-IR" dir="rtl" class="no-ie"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+	<title><?php echo $template['title'] ?></title>
+	<base href="<?php echo $template['url']; ?>" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<script type="text/javascript" defer="defer" async="async" src="script.combined.js"></script>
+</head>
+<body>
+<!--[if lt IE 9]>
+	<p class="chromeframe"><br/>برای مشاهده صحیح این وب سایت مرورگر خود را <a href="http://getbrowser.ir/" target="_blank"><b>به روز رسانی</b></a> نموده و یا <a href="http://www.chromeframe.ir/" target="_blank"><b>این پلاگین</b></a> را نصب نمایید.<br/><br/></p><br/>
+<![endif]-->
