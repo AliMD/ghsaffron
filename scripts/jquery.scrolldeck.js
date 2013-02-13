55424a8346a65c49e253136b1026d9ee16a0c983
Ali Mihandoost <i@ali.md>
2013-02-13 09:42:13 +0330

    fix right ani

--- scripts/jquery.scrolldeck.js
+++ scripts/jquery.scrolldeck.js
@@ -78,7 +78,7 @@
 						break;
 					case 'fly-in-right':
 						anim.parent().css('overflow','hidden');
-						scrolldeck.controller.animate(anim, { delay: (anim.attr('data-build')-1)*400, duration: 400, property:'right', start:-1300, pin:true });
+						scrolldeck.controller.animate(anim, { delay: (anim.attr('data-build')-1)*400, duration: 400, property:'right', start:-900, pin:true });
 						break;
 					case 'space-in':
 						scrolldeck.controller.animate(anim, { delay: (anim.attr('data-build')-1)*400, duration: 400, property:'letter-spacing', start:40, pin:true });
