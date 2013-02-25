d08cc8a00601fcd6b46a47553898ef47dff0bc82
Ali Mihandoost <i@ali.md>
2013-02-25 10:51:23 +0330

    try fix scroll bugs

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,4 @@
-function startPanel(){
-	$('div.language').animate({'bottom':'-200px'},100,function(){
-		$(this).css({'display':'none'})
-	});
-	$('div.full-bg').animate({'height':'190px'});
-	setTimeout("$('#logo').css({'display':'block'}).animate({'opacity':'0.95'})",300);
-	setTimeout("$('nav menu').animate({'top':'17px'})",400);
-	setTimeout("$.scrollTo(1800,200,'ease-in-out')",500);
-}
 
-function endPanel(){
-	$('nav menu').animate({'top':'-117px'});
-	setTimeout(function(){
-		$('#logo').animate({'opacity':'0'},400,function(){
-			$(this).css({'display':'none'});
-		});
-	},100);
-	setTimeout("$('div.full-bg').animate({'height':'825px'})",200);
-	setTimeout(function(){
-		$('div.language').css({'display':'block'}).animate({'bottom':'100px'});
-	},400);
-}
 
 
 (function($,undefined){
@@ -27,41 +6,63 @@ function endPanel(){
 	//setTimeout("$('#logo').removeClass('preload')",1000);
 	//setTimeout("$('menu').removeClass('preload')",1400);
 
+	var menu = {"#home":0,"#aboutus":400,"#aboutsaffron":1300,"#products":2580,"#product1-detail":3491,"#product2-detail":4256,"#product3-detail":5021,"#product4-detail":5786,"#product5-detail":6551,"#product6-detail":7316,"#product7-detail":8081,"#product8-detail":8846,"#certificates":9611,"#laboratory":10376,"#contactus":11091}
+
+	var startPanel = function (){
+		$('div.language').animate({'bottom':'-200px'},100,function(){
+			$(this).css({'display':'none'})
+		});
+		$('div.full-bg').animate({'height':'190px'});
+		setTimeout(function(){
+			$('#logo').css({'display':'block'}).animate({'opacity':'0.95'})
+		},300);
+		setTimeout(function(){
+			$('nav menu').animate({'top':'17px'})
+		},400);
+		setTimeout(function(){
+			$.scrollTo(menu['#aboutus'],200,'ease-in-out');
+		},500);
+	}
+
+	var endPanel = function (){
+		$('nav menu').animate({'top':'-117px'});
+		setTimeout(function(){
+			$('#logo').animate({'opacity':'0'},400,function(){
+				$(this).css({'display':'none'});
+			});
+		},100);
+		setTimeout("$('div.full-bg').animate({'height':'825px'})",200);
+		setTimeout(function(){
+			$('div.language').css({'display':'block'}).animate({'bottom':'100px'});
+		},400);
+	}
+
 	var pages = $('.pages');
 	pages.prepend('<div class="spacer"></div>');
 
 	var	deck = new $.scrolldeck({
-			buttons: 'nav a img',
-			slides: pages,
-			offset: -300
+			buttons: null,
+			slides: pages
 		});
 
-	var menu = {
-		"#home" : 0,
-		"#aboutus" : 1800,
-		"#aboutsaffron" : 4585,
-		"#products" : 10485,
-		"#product1-detail" : 13885,
-		"#product2-detail" : 16485,
-		"#product3-detail" : 19285,
-		"#product4-detail" : 22085,
-		"#product5-detail" : 24885,
-		"#product6-detail" : 27685,
-		"#product7-detail" : 30285,
-		"#product8-detail" : 33085,
-		"#certificates" : 35785,
-		"#laboratory" : 38585,
-		"#contactus" : 42495
-	}
 
 	var scrollTopage = function(linkTo){
 		$.scrollTo( menu[linkTo], 2000, 'ease-in-out');
 	}
-
+	
 	$("nav menu a,div.page-link > a,div.prds a").click(function(){
 		scrollTopage($(this).attr("href"));
 		return false;
 	});
+
+	updateScrollPos = function(){
+		for(var i in menu){
+			$.scrollTo(document.height,1);
+			$.scrollTo(i,1);
+			menu[i] = window.scrollY;
+		}
+		console.log(JSON.stringify(menu));
+	};
 
 	$('div.language a.active').click(function(){
 		startPanel();
