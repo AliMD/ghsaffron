4d1d760a4d19aabf63d1c98c9ce7f0fcf2866e97
Ali Mihandoost <i@ali.md>
2013-02-26 14:20:43 +0330

    Change intro animate and huge bugs

--- scripts/script.js
+++ scripts/script.js
@@ -4,36 +4,78 @@
 				($.browser.msie)    ? '-ms-' :
 				($.browser.opera)   ? '-o-' : '',
 
-		menu = {"#home":0,"#aboutus":400,"#aboutsaffron":1300,"#products":2580,"#certificates":9611,"#laboratory":10376,"#contactus":11091}
+		menu = {"#aboutus":400,"#aboutsaffron":1300,"#products":2580,"#certificates":9611,"#laboratory":10376,"#contactus":11091}
 
-	$(".logo-intro").css(prefix+'transform',"scale(1.4)"); // scale logo for intro
+	$(".logo-intro").css(prefix+'transform','scale(1.4)'); // scale logo for intro
 	
-	$("div.language").css({
-		'top':(window.innerHeight-400)+'px'
-	});
+	$("div.language").css('top',(window.innerHeight-400)+'px');
 
-	var startPanel = function (){
-		$.scrollTo(0,1);
-		$('div.full-bg').animate({'height':'190px'},800);
+	var rand = function(a,b){
+		if(b===undefined){
+			b=a;
+			a=0;
+		}
+		return Math.floor(Math.random()*(b-a+1))+a;
+	},
+	startPanel = function (){
+		var dly = 1;
+		$('section.flags a').each(function(){
+			$(this).delay(dly).animate({'opacity':'0'},300);
+			dly+=100;
+		});
+		$('section.flags a.active').stop(true,true).delay(500).animate({'opacity':'0'},800);
 		setTimeout(function(){
-			$('section.flags').animate({'opacity':'0'},300,function(){
-				$(this).css({'display':'none'});
-			});
-		},100);
+			$('section.flags').css('display','none');
+			continuePanel();
+		},1100);
+	},
+	continuePanel = function(){
+		$.scrollTo(0,1);
+
+		$('div.full-bg').animate({'height':'190px'},800);
+
 		setTimeout(function(){
 			//$('div.full-bg').animate({'height':'190'},400);
 			$('.language').animate({'top':'110px'},400);
 			//$(".logo-intro").removeClass("biglogo").addClass("smalllogo"); // scale(.7)
-			$(".logo-intro").css(prefix+'transform',"scale(1)");
+			$(".logo-intro").css(prefix+'transform','scale(1)');
 		},400);
+
 		setTimeout(function(){
-			$('nav menu').animate({'top':'17px'});
-		},600);
+			$('nav menu').animate({'top':'17px'},400);
+		},800);
+
 		setTimeout(function(){
 			$.scrollTo(menu["#aboutus"],300);
-		},700);
+		},900);
+
 	},
 	endPanel = function (){
+		/*
+		if(currentMenu!="#aboutus"){
+			scrollTopage("#aboutus");
+			return;
+		}
+		*/
+		
+		$.scrollTo(0,200);
+		setTimeout(function(){
+			$('nav menu').animate({'top':'-117px'},400);
+			setTimeout(function(){
+				$('div.full-bg').animate({'height':'825px'},800);
+				$('.language').animate({'top':(window.innerHeight-400)+'px'},400);
+				$(".logo-intro").css(prefix+'transform',"scale(1.4)");
+				setTimeout(function(){
+					$('section.flags').css('display','block');
+					$('section.flags a').css('opacity',0).each(function(){
+						$(this).delay(rand(1,300)).animate({'opacity':'1'},300);
+					});
+					$('section.flags a.active').stop(true,true).animate({'opacity':'1'},300);
+				},700); // 800-100
+			},300); // 400-100
+		},200); // scroll time
+
+		/*
 		$('nav menu').animate({'top':'-117px'},400);
 		setTimeout(function(){
 			$(".logo-intro").removeClass("smalllogo").addClass("biglogo"); // scale(1)
@@ -46,19 +88,20 @@
 		setTimeout(function(){
 			$('section.flags').css({'display':'block'}).animate({'opacity':'1'},300);
 		},900);
+*/
 	}
 
 
-	var currentMenu = "#aboutus";
-	var nextMenu = function(c) { //return the next menu from c
+	var currentMenu = "#aboutus",
+	nextMenu = function(c) { //return the next menu from c
 		var flag = false;
 		for(var i in menu) {
 			if(flag) break;
 			if(i===c) flag = true;
 		}
 		return i;
-	};
-	var previousMenu = function(c) { //return the previous menu from c
+	},
+	previousMenu = function(c) { //return the previous menu from c
 		var prev = "";
 		for(var i in menu) {
 			if(i===c) break;
@@ -67,18 +110,18 @@
 		return prev;
 	};
 
-	var pages = $('.pages');
-	pages.prepend('<div class="spacer"></div>');
-
-	var	deck = new $.scrolldeck({
+	var pages = $('.pages'),
+	deck = new $.scrolldeck({
 		buttons: null,
 		slides: pages
 	});
+	pages.prepend('<div class="spacer"></div>');
 
 
 	var scrollTopage = function(linkTo){
-		$.scrollTo( menu[linkTo], 1500, 'easeOutExpo');
 		currentMenu = linkTo;
+		if(!menu[linkTo])return;
+		$.scrollTo( menu[linkTo], 1500, 'easeOutExpo');
 	}
 
 	
