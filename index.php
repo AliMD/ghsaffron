c900bdc6464d8c9087c91f5adee55ed53c27f2d6
Ali aGHasizadeh <me@aligh.pro>
2013-05-25 10:16:25 +0430

    remove underDev

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE);
 
-session_start();
-
-isset($_GET['debug']) and $_SESSION['debug']=!!$_GET['debug'];
-
-if(!$_SESSION['debug']){
-	header("Location: underdev/");
-	exit();
-}else{
 	header("Location: home_fa.html");
-}
 
